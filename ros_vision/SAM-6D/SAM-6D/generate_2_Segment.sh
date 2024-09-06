872f119c63141bd665d7126d62a3c2bb912f9016
KKXiaoKang <410080663@qq.com>
2024-09-06 17:29:10 +0800

    feat:修改分割模型从fastsam到sam

--- ros_vision/SAM-6D/SAM-6D/generate_2_Segment.sh
+++ ros_vision/SAM-6D/SAM-6D/generate_2_Segment.sh
@@ -1,5 +1,6 @@
 # Run instance segmentation model
 export SEGMENTOR_MODEL=sam
+#export SEGMENTOR_MODEL=fastsam
 
 cd ./Instance_Segmentation_Model
 python run_inference_custom.py --segmentor_model $SEGMENTOR_MODEL --output_dir $OUTPUT_DIR --cad_path $CAD_PATH --rgb_path $RGB_PATH --depth_path $DEPTH_PATH --cam_path $CAMERA_PATH
